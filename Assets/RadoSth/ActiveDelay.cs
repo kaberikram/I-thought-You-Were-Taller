c2c075129576530bd44707f012a4932700fc49be
Radoooooo <84391132+Radoooooo@users.noreply.github.com>
2021-05-26 17:56:31 +0200

    AmiationTrack
    HOpe it will work:D

---
+++ Assets/RadoSth/ActiveDelay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveDelay : MonoBehaviour
+{
+  public float delay = 30;
+  public GameObject FotonRed;
+	
+	void Start()
+ {
+ StartCoroutine(LateCall());
+ }
+ 
+		IEnumerator LateCall()
+     {
+ 
+        yield return new WaitForSeconds(delay);
+		 
+		FotonRed.SetActive(true);
+	 }
+}
